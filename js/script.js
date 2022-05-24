cdca375ca04a62311b3569c7a29b2c47eafd4ccf
TiagoNatalio <tiagonatalio41@gmail.com>
2022-05-24 00:52:54 -0300

    conectei com o formulario

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function success_pf() {
+    alert("Cadastro de Pessoa Física Realizado com Sucesso");
+}
+
+function success_pj() {
+    alert("Cadastro de Pessoa Jurídica Realizado com Sucesso");
+}
+
+
